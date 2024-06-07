6acf0dbd1493f814b5f4cf6f3e18de241e3ac657
omerbag-9 <omerbag2002@gmail.com>
2024-06-08 02:52:23 +0300

    third commit

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('minifacebook','root','',{
-    host:'localhost',
-    dialect:'mysql'
-})
+export const sequelize = new Sequelize(`mysql://ulrxllwjk6l8t1ns:mFw8OKEZrMbWoYPpqBvL@bavkgngzdlkitf9qvx2m-mysql.services.clever-cloud.com:3306/bavkgngzdlkitf9qvx2m`)
 
 export const connectDB = ()=>{sequelize.authenticate().then(()=>{console.log("db connected successfully");})
 .catch(()=>{console.log("failed to connect to db");})
